9577119408afde3a61a0f670c2680ad62ade6128
Alejandro Morales Sierra <alejandro.moralessierra@wur.nl>
2023-08-24 10:13:42 +0200

    Fix dependencies, add compat and Aqua tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,16 @@
 using VPLRayTracer
 using Test
+import Aqua
 
 @testset "VPLRayTracer.jl" begin
+
+    # Aqua
+    @testset "Aqua" begin
+        Aqua.test_all(VPLRayTracer, ambiguities = false)
+        Aqua.test_ambiguities([VPLRayTracer])
+    end
+
+
     include("elements_raytracer.jl")
     #include("scenes_raytracer.jl")
 end
